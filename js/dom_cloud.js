4e4f59fb94c6cff254907e2a1a30f6f3bd25b33f
kwhitejr <kwhitejr.edu@gmail.com>
2015-12-10 22:05:59 -1000

    Attempted bookmarklet. Failed :(

--- js/dom_cloud.js
+++ js/dom_cloud.js
@@ -1,10 +1,8 @@
-window.onload = function() {
+// window.onload = function() {
 
   var elCounter = {}; // this will be an objects of element/attribute keys and counter values {div: 5, head: 1}
 
   function getMyElements(node) {
-    console.log("Let's get started: ");
-
     // Collect nodeName, send to counter
     if (!(elCounter[node.nodeName])) {
       elCounter[node.nodeName] = 1;
@@ -28,16 +26,13 @@ window.onload = function() {
       while (child) {
         if (child.nodeType === 1) {
           getMyElements(child);
-          console.log('...unwinding...');
         }
         child = child.nextSibling;
       }
     }
 
   }
-  console.log('unwinded');
   getMyElements(document.body);
-  console.log(elCounter);
 
   var sortedCounters = []; // this will be an array of all captured elements and attributes
 
@@ -47,32 +42,18 @@ window.onload = function() {
 
   // sort captured elements/attributes by counter, beginning with highest
   sortedCounters.sort(function(a, b) { return b[1] - a[1]; });
-  console.log(sortedCounters);
 
   // smaller array of only the top twenty
   var topTwenty = sortedCounters.slice(0, 20);
-  console.log(topTwenty);
 
-  var domCloudContainer = document.getElementById('dom_cloud_container');
+  // send information to the domCloudContainer. Font-size is relative to the incidence of the respective element/attribute.
+  var thisBody = document.getElementsByTagName('body');
   topTwenty.forEach(function (item) {
-    domCloudContainer.innerHTML += "<span style=\"font-size:" + item[1] + "\">" +
+
+    thisBody.appendChild("<span style=\"font-size:" + item[1] + "\">" +
     "This page contains " +
     item[1] + " elements/attributes of the type \"" +
-    item[0] + "\".</span><br>";
+    item[0] + "\".</span><br>");
   });
 
-};
-
-/*
-if (element.hasChildNodes()) {}; // returns true if element has at least one child
-
-if (element.firstChild) {}; // returns true if element has at least one child
-
-.attributes : returns a live collection of all attribute nodes registered to the specified node
-
-.children
-
-.nodeName & .tagName each return a string of a current node/element
-
-
-*/
+// };
---
+++ js/elementCounterBookmarklet.js
@@ -0,0 +1,5 @@
+javascript: (function () {
+    var jsCode = document.createElement('script');
+    jsCode.setAttribute('src', 'http://0.0.0.0:8080/js/dom_cloud.js');
+  document.body.appendChild(jsCode);
+ }());
